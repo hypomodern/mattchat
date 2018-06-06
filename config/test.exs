6f25dc2f14b346d8034f678847e31a5671dc3d94
Matt Wilson <mhw@hypomodern.com>
2018-06-06 11:16:55 -0400

    Minimal DB config to get server booted

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :mattchat, Mattchat.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "mhw",
+  password: "",
   database: "mattchat_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
